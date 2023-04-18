c748d4769c091570429449631c7dbe629714eeea
Your Name <you@example.com>
2023-04-19 00:35:18 +0530

    COMMIT

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import Navbar from './components/navbar/Navbar'
+import './globals.css'
+import { Nunito } from "next/font/google"
+
+export const metadata = {
+  title: 'Fleet',
+  description: 'Airbnb clone',
+}
+
+const font = Nunito({
+  subsets: ['latin']
+})
+
+export default function RootLayout({ children }: { children: React.ReactNode }) 
+{
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  )
+}
